b7113aacaab63468e4f091a0d0ecad1f891e8d0b
Gabe Deyo <gdeyo@ibidata.com>
2017-02-09 16:01:51 -0600

    Next Module

--- Equality/Program.cs
+++ Equality/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Equality {
+﻿using System;
+
+namespace Equality {
 	class Program {
 		static void Main(string[] args) {
+			Food banana = new Food("banana");
+			Food banana2 = new Food("banana");
+			Food chocolate = new Food("chocolate");
 
+			//IEquatable Demo
+
+			//.Equals(banana2) is string's implementation of IEquatable<string>
+			Console.WriteLine(banana.Equals(banana2));
+
+			/* 2 methods 
+				1. The overridden object.Equals method
+				2. The strongly typed overload is int's implementation of IEquatable<int>
+			*/
+			int three = 3;
+			three.Equals(3);
 		}
 	}
 }
